5534b43d1ca702538c372df6d34c29b39eb57aae
Vivien Bunane <devvien.xyz@gmail.com>
2025-04-22 19:17:22 +0800

    Added @tailwindcss vite and README.md for pixel adventure

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 npm install -g pnpm  # [optional] Skip if pnpm is already installed.
 pnpm install
 ```
+
+## Development
+
+```bash
+pnpm dev
+
+# To add a package to dev dependencies, do:
+pnpm add -D <package_name>
+
+# add -w to add to turbo workspace instead of local
+# eg: pnpm add -D @tailwindcss/vite -w
+```
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,72 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-	plugins: [react()],
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [
+			react(),
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest: {
+					name: "Devvienxyz's Portfolio",
+					short_name: "devvienxyz-portfolio",
+					start_url: "/",
+					display: "standalone",
+					theme_color: "#ff385c",
+					background_color: "#ffffff",
+					icons: [
+						{
+							src: "/icons/icon-192x192.png",
+							sizes: "192x192",
+							type: "image/png",
+						},
+						{
+							src: "/icons/icon-512x512.png",
+							sizes: "512x512",
+							type: "image/png",
+						},
+					],
+				},
+			}),
+			tailwindcss(),
+		],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "apps"),
+				"@assets": path.resolve(__dirname, "assets"),
+				"@packages": path.resolve(__dirname, "packages"),
+				"@controller": path.resolve(__dirname, "apps/controller"),
+				"@pixel": path.resolve(__dirname, "apps/pixeladventure"),
+			},
+		},
+		build: {
+			sourcemap: false, // Disable source maps in production
+			outDir: "dist",
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return "vendor";
+						}
+					},
+				},
+			},
+		},
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_API_BASE_URL || "http://backend:8000",
+					changeOrigin: true,
+				},
+			},
+		},
+	};
 });
