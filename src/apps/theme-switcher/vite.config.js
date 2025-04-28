582a4463e1ecefa9ef0be1ecea3710d7f296e107
Vivien Bunane <devvien.xyz@gmail.com>
2025-04-28 18:08:35 +0800

    Initialized theme switcher files

---
+++ src/apps/theme-switcher/vite.config.js
@@ -0,0 +1,9 @@
+import baseConfig from "@shared/config/vite.base";
+import { mergeConfig } from "vite";
+
+export default mergeConfig(baseConfig, {
+	root: __dirname,
+	build: {
+		outDir: "dist",
+	},
+});
