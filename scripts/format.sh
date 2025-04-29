a330801b5422fe9539e3db01e4d66799a0de2384
Vivien Bunane <devvien.xyz@gmail.com>
2025-04-29 14:36:37 +0800

    Specified node 22 version to fix punycode deprecation warning; Add ignored files in biome.json config

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,22 +2,27 @@
 
 format_check_all() {
   echo "Checking all files for formatting..."
-  biome format --check --ignore '**/node_modules/**'
+  biome format
 }
 
 format_check_staged() {
   echo "Checking staged files for formatting..."
-  git diff --cached --name-only --diff-filter=ACM | grep '\\.jsx$' | xargs biome format --ignore '**/node_modules/**'
+  git diff --cached --name-only --diff-filter=ACM \
+    | grep '\.jsx$' \
+    | grep -vE '^dist/|/dist/|^temp/|/temp/|^node_modules/|/node_modules/' \
+    | xargs biome format
 }
 
 format_fix_all() {
   echo "Fixing all files..."
-  biome format --fix --ignore '**/node_modules/**'
+  biome format --fix
 }
 
 format_fix_staged() {
   echo "Fixing staged files..."
-  git diff --cached --name-only --diff-filter=ACM | grep '\\.jsx$' | xargs biome format --fix --ignore '**/node_modules/**'
+  git diff --cached --name-only --diff-filter=ACM \
+    | grep '\.jsx$' \
+    | xargs biome format --fix
 }
 
 # Check if any of the functions are called
