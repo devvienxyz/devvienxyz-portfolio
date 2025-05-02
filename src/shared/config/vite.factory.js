0ddada9ecfd7ee50e95a7c46e519ff5a8e809d61
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-02 15:05:53 +0800

    Added rollup plugin visualizer

--- src/shared/config/vite.factory.js
+++ src/shared/config/vite.factory.js
@@ -2,6 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import baseAliases, { rootDir } from "./baseAliases.js";
@@ -53,6 +54,7 @@ export const createAppConfig = ({ appName, alias = {}, pwaManifest = {}, base = 
 							}
 						},
 					},
+					plugins: [visualizer({ open: true, filename: "dist/stats.html" })],
 				},
 			},
 		};
