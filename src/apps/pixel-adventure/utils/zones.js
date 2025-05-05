b288e82da6ed1229bf3a1cc34ab25feff4e3ae77
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-05 23:29:27 +0800

    Add initial zone and added placeholder for target zoom focus

--- src/apps/pixel-adventure/utils/zones.js
+++ src/apps/pixel-adventure/utils/zones.js
@@ -1,6 +1,7 @@
 import { GROUND_LEVEL } from "../constants/terrain-misc";
 
 const ZoneNames = {
+	INITIAL: "initial",
 	ABOUT: "about",
 	PROJECTS: "projects",
 	EXPERIENCES: "experiences",
@@ -8,26 +9,44 @@ const ZoneNames = {
 };
 
 const Zones = Object.freeze({
+	[ZoneNames.INITIAL]: {
+		bounds: { minX: -3.0, maxX: -2.0, minZ: 1.0, maxZ: 3.0 },
+		initialPoints: [-2.5, GROUND_LEVEL, 1.1],
+		// building-port
+		targetZoomFocus: [-2.5, 0, 0.7],
+		preRequisiteZone: 0,
+	},
 	[ZoneNames.ABOUT]: {
 		bounds: { minX: -3.0, maxX: -2.0, minZ: 1.0, maxZ: 3.0 },
 		initialPoints: [-2.5, GROUND_LEVEL, 1.1],
-		preRequisiteZone: 0,
+		// building-mill
+		targetZoomFocus: [-1.5, 0.2, -0.8],
+		preRequisiteZone: 1,
 	},
 	[ZoneNames.PROJECTS]: {
 		bounds: { minX: -3.5, maxX: 3, minZ: -5.5, maxZ: 1.9 },
-		preRequisiteZone: 1,
+		initialPoints: [-2.5, GROUND_LEVEL, 1.1],
+		// building-archery
+		targetZoomFocus: [-0.5, 0, -2.8],
+		preRequisiteZone: 2,
 	},
 	[ZoneNames.EXPERIENCES]: {
 		bounds: { minX: -3.5, maxX: 3, minZ: -5.5, maxZ: 1.9 },
-		preRequisiteZone: 2,
+		initialPoints: [-2.5, GROUND_LEVEL, 1.1],
+		// stone-mountain
+		targetZoomFocus: [3, 0, -1.9],
+		preRequisiteZone: 3,
 	},
 	[ZoneNames.CREDITS]: {
 		bounds: { minX: -3.5, maxX: 3, minZ: -5.5, maxZ: 1.9 },
-		preRequisiteZone: 3,
+		initialPoints: [-2.5, GROUND_LEVEL, 1.1],
+		// building-wizard-tower
+		targetZoomFocus: [3, 0, 2.9],
+		preRequisiteZone: 4,
 	},
 	map: {
 		bounds: { minX: -3.5, maxX: 4, minZ: -3.8, maxZ: 3.6 },
-		preRequisiteZone: 4,
+		preRequisiteZone: 5,
 	},
 });
 
