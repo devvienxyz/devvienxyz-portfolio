d064280eb751d19ddfd342e3a34f789465981d74
Vivien Bunane <devvien.xyz@gmail.com>
2025-09-29 14:43:34 +0800

    feat(Base-Clickable): Base form of Clickable3d Object

---
+++ src/apps/pixel-adventure/components/Map-Builder/Base-Clickable.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function BaseClickable({ onClick, position }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      onClick?.();
+    }
+  };
+
+  return (
+    <mesh
+      tabIndex={0} // Makes it focusable
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      position={position}
+    >
+      BaseClickable
+    </mesh>
+  );
+}
