537ce32be8afb1ddca024df0f10905cce918b673
Vivien Bunane <devvien.xyz@gmail.com>
2025-09-29 14:44:37 +0800

    chore: Updated barrel exports of hooks

--- src/apps/pixel-adventure/hooks/index.js
+++ src/apps/pixel-adventure/hooks/index.js
@@ -1,1 +1,3 @@
+export { default as useAvatar } from "./useAvatar.js";
 export { default as useCameraAnimation } from "./useCameraAnimation.js";
+export { default as useMovementController } from "./useMovementController.js";
