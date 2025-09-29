11e5bb87b78a0af5bcb655483a144ce892c4d072
Vivien Bunane <devvien.xyz@gmail.com>
2025-09-29 15:01:12 +0800

    feat(avatar): Sync global avatar state with useMovementController

--- src/apps/pixel-adventure/components/Avatar/Avatar.jsx
+++ src/apps/pixel-adventure/components/Avatar/Avatar.jsx
@@ -1,14 +1,16 @@
-import Avatars, { AVATAR_SCALE } from "@pixel/constants/avatar-attributes.js";
-import useMovementController from "@pixel/hooks/useMovementController.js";
-import { Zones } from "@pixel/utils/zones.js";
-import { useAnimations, useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { Zones } from "@pixel/utils/zones.js";
+import Avatars, { AVATAR_SCALE } from "@pixel/constants/avatar-attributes.js";
+import { useMovementController, useAvatar } from "@pixel/hooks";
 
 export default function Avatar() {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF(`assets/kenney/characters/Models/GLB format/${Avatars.default}.glb`);
+  const { nodes, animations } = useGLTF(`assets/kenney/characters/Models/GLB format/${Avatars.default}.glb`);
   const { actions } = useAnimations(animations, group);
+
+  const avatar = useAvatar();
 
   const controller = useMovementController({
     onMove: (vel, dir) => {
@@ -18,9 +20,14 @@ export default function Avatar() {
       if (dir.lengthSq() > 0) {
         group.current.rotation.y = Math.atan2(dir.x, dir.z);
       }
+
+      // keep global avatar state in sync
+      avatar._update(group.current.position, dir);
     },
     actions,
   });
+
+  // update animations
   useFrame((_, delta) => {
     controller.update(delta, group.current.position);
   });
