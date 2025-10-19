b592085b1af61088066b3e1d56132d918928d458
Vivien Bunane <devvien.xyz@gmail.com>
2025-10-19 21:13:31 +0800

    feat(Zones): constants for zone actions -- on hover, on enter

---
+++ src/apps/pixel-adventure/constants/zone-actions.js
@@ -0,0 +1,15 @@
+const OnHoverZoneActions = Object.freeze({
+  SHOW_ABOUT_BANNER: "show_about_banner",
+  SHOW_PROJECTS_BANNER: "show_projects_banner",
+  SHOW_WORK_BANNER: "show_work_banner",
+  SHOW_CREDITS_BANNER: "show_credits_banner",
+});
+
+const OnEnterZoneActions = Object.freeze({
+  SHOW_ABOUT_2D_MAP: "show_about_2d_map",
+  SHOW_PROJECTS_2D_MAP: "show_projects_2d_map",
+  SHOW_WORK_2D_MAP: "show_work_2d_map",
+  SHOW_CREDITS_2D_MAP: "show_credits_2d_map",
+});
+
+export { OnHoverZoneActions, OnEnterZoneActions };
